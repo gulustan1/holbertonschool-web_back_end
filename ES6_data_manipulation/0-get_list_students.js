ec766526d0a5385cbf4f58f402165e1767f6844e
gulustan1 <gulustanaydinn@gmail.com>
2026-07-17 12:03:58 +0000

    update

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+/**
+ * Retrieves a basic list of student objects.
+ * @returns {Array<{id: number, firstName: string, location: string}>} The array of student objects.
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+}
---
+++ ES6_data_manipulation/README.md
@@ -0,0 +1,1 @@
+ndjdjdj
