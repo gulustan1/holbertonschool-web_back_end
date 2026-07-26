77f6f1117e313d17405dff158fe9e17f0fb38b1f
gulustan1 <gulustanaydinn@gmail.com>
2026-07-26 18:17:08 +0000

    update

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,5 +1,12 @@
-const displayMessage = (message) => {
-  console.log(message);
-};
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-export default displayMessage;
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
